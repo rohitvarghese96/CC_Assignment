ed26a02209e1797aafd7ca35efb4848a08820ac9
Rohit Varghese <rohitvarghese96@gmail.com>
2021-04-15 22:14:53 +0530

    Code Upload

---
+++ Assignment_2/MapReduceProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MapReduceProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            //Hadoop passes data to the mapperon STDIN
+            while((line = Console.ReadLine()) != null)
+            {
+                //Only words. Strip punctuation
+                var onlyText = Regex.Replace(line, @"\.|;|:|,|[0-9]|'", "");
+
+                //Split at whitespace
+                var words = Regex.Matches(onlyText, @"[\w]+");
+
+                //Loop over the words
+                foreach(var word in words)
+                {
+                    //Emit tab-delimited key/value pairs
+                    Console.WriteLine("{0}\t1", word);
+                }
+            }
+        }
+    }
+}
---
+++ Assignment_2/Reducer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary for holding count of words
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            string line;
+
+            //Read from STDIN
+            while((line = Console.ReadLine()) != null)
+            {
+                //Data from Hadoop is tab/de-limited key/value pairs
+                var sArr = line.Split('\t');
+
+                //Get the word
+                string word = sArr[0];
+
+                //Get the count
+                int count = Convert.ToInt32(sArr[1]);
+
+                //If count already exist
+                if (words.ContainsKey(word))
+                {
+                    //Increment the count
+                    words[word] += count;
+                }
+                else
+                {
+                    //Add key to the collection
+                    words.Add(word, count);
+                }
+            }
+
+            //Finally, emit each word and count
+            foreach(var word in words)
+            {
+                //Emit tab de-limited key/value pairs
+                Console.WriteLine("{0}\t{1}", word.Key, word.Value);
+            }
+        }
+    }
+}
